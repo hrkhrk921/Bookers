d680d03850c45360813fa54ad790ad0bd6a3fd99
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-04-29 15:27:40 +0900

    エラーメッセージ表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,10 +19,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -33,10 +36,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to  book_path(book.id)
+      redirect_to  book_path(@book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
   private
    def book_params
